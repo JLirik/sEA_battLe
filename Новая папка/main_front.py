b65a6cbb2045f207efb878768768463b8be44526
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-01-19 14:50:40 +0300

    Update main_front.py

--- Новая папка/main_front.py
+++ Новая папка/main_front.py
@@ -4,22 +4,22 @@ from sqll import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'predpof_code_crusaders'
-init_database('predprof.db')
+init_database()
 
 
 @app.route('/', methods=['GET'])
 def first():
-    return render_template('first_page.html', style=url_for('static', filename='css/style.css'))
+    return render_template('first_page.html', style=url_for('static', filename='css/css_for_reg.css'))
 
 
 @app.route('/reg', methods=['GET'])
 def reg():
-    return render_template('registration_form.html', style=url_for('static', filename='css/style.css'))
+    return render_template('registration_unit.html')
 
 
 @app.route('/admin/main', methods=['GET'])
 def admin_main():
-    return render_template('admin_main.html', style=url_for('static', filename='css/style.css'))
+    return render_template('admin_main.html', style=url_for('static', filename='css/css_for_reg.css'))
 
 
 @app.route('/admin/create_field', methods=['GET', 'POST'])
@@ -29,12 +29,13 @@ def admin_create_field():
         # add_field()
         pass
     else:
-        return render_template('admin_create_field.html', style=url_for('static', filename='css/style.css'), form=form)
+        return render_template('admin_create_field.html',
+                               style=url_for('static', filename='css/css_for_reg.css'), form=form)
 
 
 @app.route('/user/main', methods=['GET, POST'])
 def user_main():
-    return render_template('user_main.html', style=url_for('static', filename='css/style.css'))
+    return render_template('user_main.html', style=url_for('static', filename='css/css_for_reg.css'))
 
 
 if __name__ == '__main__':
