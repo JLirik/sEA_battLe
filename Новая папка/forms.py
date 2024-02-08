1b1452712e98d33557099aa208466ba5edabfca1
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-02-09 02:30:45 +0300

    Delete Новая папка/forms.py

--- Новая папка/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class RegForm(FlaskForm):
-    cod = StringField('Введите код для скачивания:', validators=[DataRequired()])
-    download = SubmitField('Скачать')
